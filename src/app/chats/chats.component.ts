b9b4bf16dc2724b00f97b53798f82214d9330030
Rima Abou Chacra <94137881+rimaabouchacra@users.noreply.github.com>
2024-04-21 17:44:51 +0300

    added chats style

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -16,22 +16,19 @@ export class ChatsComponent {
   }
 
   fetchChats() {
-    // Retrieve token from local storage
     const token = localStorage.getItem('token');
     if (!token) {
       console.error('Token not found in local storage');
       return;
     }
 
-    // Include token in the request headers
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    // Make the HTTP request with the authorization token
     this.http
-      .get<any[]>('http://localhost:8000/api/v1/chats', { headers })
+      .get<any[]>('http://localhost:8000/api/v1/chats/last', { headers })
       .subscribe(
         (response) => {
           this.chats = response;
+          console.log(response)
         },
         (error) => {
           console.error('Error fetching chats:', error);
